4d93c4d87e8b42bd36a60d3d47b06d49e4eeea55
Rotaru Development <77880157+ramona232@users.noreply.github.com>
2021-04-28 10:45:34 +0200

    Add files via upload

---
+++ gui/butones.py
@@ -0,0 +1,23 @@
+from tkinter import*
+
+root = Tk()
+
+def myClick(): 
+   myLabel= Label(root, text="Look! I clicked a button!")
+   myLabel.pack()
+
+myButton = Button(root, text="Click me!", command=myClick, fg="yellow", bg="red" )
+myButton.pack()
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
